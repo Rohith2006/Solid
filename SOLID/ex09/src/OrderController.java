0b59f0f46ade7f50f37e7ba593440071a970d009
Rohith Thirunagari <trohith.2006@gmail.com>
2025-08-25 15:59:48 +0530

    complete 9

--- SOLID/ex09/src/OrderController.java
+++ SOLID/ex09/src/OrderController.java
@@ -1,6 +1,12 @@
 public class OrderController {
+
+    private OrderRepository repo;
+
+    public OrderController(OrderRepository repo) {
+        this.repo = repo;
+    }
+
     void create(String id, double amount){
-        OrderRepository repo = new SqlOrderRepository();
         repo.save(new Order(id, amount));
         System.out.println("Created order: " + id);
     }
